93568b24bdcea69140ef649942a9f77f74fdff0a
lockerfish <htavarez@gmail.com>
2013-04-24 09:46:15 -0400

    Wire up a backend

---
+++ js/mongolab.js
@@ -0,0 +1,22 @@
+// This is a module for cloud persistence in mongolab.com
+var mongo = angular.module('mongolab', ['ngResource']);
+
+mongo.factory('Project', function ($resource) {
+	var Project = $resource('https://api.mongolab.com/api/1/databases' +
+          '/angularjs/collections/projects/:id',
+          { apiKey: '2tAJ-CIssogfT683gTCqLNEOyDxy3ALA' },  {
+          	update: {method: 'PUT'} 
+          }
+    );
+
+    Project.prototype.update = function(cb) {
+    	return Project.update({id: this._id.$oid},
+            angular.extend({}, this, {_id:undefined}), cb);
+    };
+
+    Project.prototype.destroy = function(cb) {
+    	return Project.remove({id: this._id.$oid}, cb);
+    };
+
+    return Project;
+})
